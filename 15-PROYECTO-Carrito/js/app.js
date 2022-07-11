72cb63ea917b3885687eb4e69cde7183962ee283
JoseLuisUicab <joseluisuicabcan30@gmail.com>
2022-07-11 17:47:39 -0500

    dos

--- 15-PROYECTO-Carrito/js/app.js
+++ 15-PROYECTO-Carrito/js/app.js
@@ -3,6 +3,7 @@ const carrito = document.querySelector("#carrito");
 const contenedorcarrito = document.querySelector("#lista-carrito tbody");
 const vaciarcarrritoBTN = document.querySelector("#vaciar-carrito");
 const listacurso = document.querySelector("#lista-cursos");
+let articulos = [];
 
 cargarEventListener();
 
@@ -11,10 +12,25 @@ function cargarEventListener() {
 }
 function agregarproducto(e) {
   e.preventDefault();
-  const selccionar = e.
+
   if (e.target.classList.contains("agregar-carrito")) {
-    console.log("ggggggggggggg");
+    const selccionar = e.target.parentElement.parentElement;
+    leerProducto(selccionar);
   }
+}
+
+// leer contenido del html al que se le hace clICK y extrar informacion
+function leerProducto(curso) {
+  // ojeto parea elselccionar contenido actaul
+  const informacion = {
+    imagen: curso.querySelector("img").src,
+    titulo: curso.querySelector("h4").textContent,
+    precio: curso.querySelector(".precio span").textContent,
+    id: curso.querySelector("a").getAttribute("data-id"),
+    cantidad: 1,
+  };
+  // agregar elementos al carrito
+  articulos = [...articulos, informa];
 }
 
 // leer contenido del html para extrar la informacion
